2c5c59e422c915be30f38aecae490643a04373fc
andresantiagohellmann <andresantiagohellmann@gmail.com>
2021-12-06 10:23:50 -0300

    start with chakra

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
-const foo = 'bla'
+import { Box, Center, Flex, Square, Text } from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Flex color="white">
+      <Center w="100px" bg="green.500">
+        <Text>home</Text>
+      </Center>
+      <Square bg="blue.500" size="150px">
+        <Text>with</Text>
+      </Square>
+      <Box flex="1" bg="tomato">
+        <Text>chakra</Text>
+      </Box>
+    </Flex>
   )
 }
